ca9ce054d7f60a2590e9e12ff994e7654bbb9c29
Corpella <alessandrocordella97@gmail.com>
2022-06-25 19:41:41 +0200

    feat: script setup, aspect ratio

--- src/components/Bans.vue
+++ src/components/Bans.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-full flex justify-center mt-5" :class="{ 'flex-row-reverse': props.side == 'blue' }">
-        <div v-for="i in 5" :key="i" class="ban-box relative bg-black border-2 border-gray-400 mr-2">
+        <div v-for="i in 5" :key="i" class="w-10 aspect-square relative bg-black border-2 border-gray-400 mr-2">
             <div v-if="checkBan(i - 1)">
-                <img :key="i" :src="props.bans[i - 1].image" :alt="props.bans[i - 1].name" />
+                <img :key="i" :src="bans?.[i - 1].image" :alt="bans?.[i - 1].name" />
             </div>
             <div class="w-2/3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" v-else>
                 <BanIcon />
@@ -11,30 +11,17 @@
     </div>
 </template>
 
-<script lang="ts">
-import { Champions, Side } from "@/types/championSelect"
-import { defineComponent, PropType } from "vue"
+<script lang="ts" setup>
+import { PropType } from "vue"
+import { Champions, Side } from "@/types/championSelect.types"
 import BanIcon from "./icons/BanIcon.vue"
 
-export default defineComponent({
-    name: "Bans",
-    components: { BanIcon },
-    props: {
-        bans: Array as PropType<Champions>,
-        side: String as PropType<Side>,
-    },
-    setup(props) {
-        const checkBan = (index: number): string | null | undefined => {
-            return props.bans ? props.bans[index]?.name : null
-        }
-        return { props, checkBan }
-    },
+const props = defineProps({
+    bans: Array as PropType<Champions>,
+    side: String as PropType<Side>,
 })
-</script>
 
-<style lang="scss" scoped>
-.ban-box {
-    width: 40px;
-    height: 40px;
+const checkBan = (index: number): string | null | undefined => {
+    return props.bans ? props.bans[index]?.name : null
 }
-</style>
+</script>
