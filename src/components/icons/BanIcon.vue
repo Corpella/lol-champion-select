afa4683e63e4c01d17e05868a050b5130fda322b
Corpella <alessandrocordella97@gmail.com>
2022-06-25 18:16:08 +0200

    feat: script setup in icons, aspect ratio

--- src/components/icons/BanIcon.vue
+++ src/components/icons/BanIcon.vue
@@ -1,41 +1,30 @@
 <template>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    :stroke="iconColor"
-    :width="iconWidth"
-    :height="iconHeight"
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      stroke-width="2"
-      d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
-    />
-  </svg>
+    <svg
+        :width="width"
+        :stroke="color"
+        class="aspect-square"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+    >
+        <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"
+        />
+    </svg>
 </template>
 
-<script lang="ts">
-import { defineComponent, PropType } from 'vue'
+<script lang="ts" setup>
+import { PropType } from "vue"
 
-export default defineComponent({
-  name: 'HoverIcon',
-  props: {
+defineProps({
     width: Number as PropType<number>,
-    height: Number as PropType<number>,
     color: {
-      type: String as PropType<string>,
-      default: 'gray',
+        type: String as PropType<string>,
+        default: "gray",
     },
-  },
-  setup(props) {
-    return {
-      iconWidth: props.width,
-      iconHeight: props.height,
-      iconColor: props.color,
-    }
-  },
 })
 </script>
 
