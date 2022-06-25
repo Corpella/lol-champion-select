55b8e1fa8dd7c1eae5ffe0ba3187a39bc4eee5da
Corpella <alessandrocordella97@gmail.com>
2022-06-25 17:11:01 +0200

    chore: updated to pnpm, updated to tailwind3

--- src/components/Bans.vue
+++ src/components/Bans.vue
@@ -1,54 +1,40 @@
 <template>
-  <div
-    class="w-full flex justify-center mt-5"
-    :class="{ 'flex-row-reverse': props.side == 'blue' }"
-  >
-    <div
-      v-for="i in 5"
-      :key="i"
-      class="ban-box relative bg-black border-2 border-gray-400 mr-2"
-    >
-      <div v-if="checkBan(i - 1)">
-        <img
-          :key="i"
-          :src="props.bans[i - 1].image"
-          :alt="props.bans[i - 1].name"
-        />
-      </div>
-      <div
-        class="w-2/3 absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-        v-else
-      >
-        <BanIcon />
-      </div>
+    <div class="w-full flex justify-center mt-5" :class="{ 'flex-row-reverse': props.side == 'blue' }">
+        <div v-for="i in 5" :key="i" class="ban-box relative bg-black border-2 border-gray-400 mr-2">
+            <div v-if="checkBan(i - 1)">
+                <img :key="i" :src="props.bans[i - 1].image" :alt="props.bans[i - 1].name" />
+            </div>
+            <div class="w-2/3 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2" v-else>
+                <BanIcon />
+            </div>
+        </div>
     </div>
-  </div>
 </template>
 
 <script lang="ts">
-import { Champions, Side } from '@/types/championSelect'
-import { defineComponent, PropType } from 'vue'
-import BanIcon from './icons/BanIcon.vue'
+import { Champions, Side } from "@/types/championSelect"
+import { defineComponent, PropType } from "vue"
+import BanIcon from "./icons/BanIcon.vue"
 
 export default defineComponent({
-  name: 'Bans',
-  components: { BanIcon },
-  props: {
-    bans: Array as PropType<Champions>,
-    side: String as PropType<Side>,
-  },
-  setup(props) {
-    const checkBan = (index: number): string | null | undefined => {
-      return props.bans ? props.bans[index]?.name : null
-    }
-    return { props, checkBan }
-  },
+    name: "Bans",
+    components: { BanIcon },
+    props: {
+        bans: Array as PropType<Champions>,
+        side: String as PropType<Side>,
+    },
+    setup(props) {
+        const checkBan = (index: number): string | null | undefined => {
+            return props.bans ? props.bans[index]?.name : null
+        }
+        return { props, checkBan }
+    },
 })
 </script>
 
 <style lang="scss" scoped>
 .ban-box {
-  width: 40px;
-  height: 40px;
+    width: 40px;
+    height: 40px;
 }
 </style>
