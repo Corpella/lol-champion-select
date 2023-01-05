0ecc8b06f57d637a723651cd2216a6acacbf2e1c
Corpella <alessandrocordella97@gmail.com>
2023-01-05 23:05:26 +0100

    feat: move script as first tag, improve accessibility

--- src/components/Bans.vue
+++ src/components/Bans.vue
@@ -1,19 +1,6 @@
-<template>
-    <div class="mt-5 flex w-full justify-center" :class="{ 'flex-row-reverse': side == 'blue' }">
-        <div v-for="i in 5" :key="i" class="relative mr-2 aspect-square w-10 border-2 border-gray-400 bg-black">
-            <div v-if="checkBan(i - 1)">
-                <img :key="i" :src="bans?.[i - 1].image" :alt="bans?.[i - 1].name" />
-            </div>
-            <div class="absolute top-1/2 left-1/2 w-2/3 -translate-x-1/2 -translate-y-1/2" v-else>
-                <BanIcon />
-            </div>
-        </div>
-    </div>
-</template>
-
 <script lang="ts" setup>
-import { PropType } from "vue"
-import { Champions, Side } from "@/types/championSelect.types"
+import { computed, PropType } from "vue"
+import { Champions, Side } from "~/championSelect"
 import BanIcon from "./icons/BanIcon.vue"
 
 const props = defineProps({
@@ -21,7 +8,29 @@ const props = defineProps({
     side: String as PropType<Side>,
 })
 
-const checkBan = (index: number): string | undefined => {
-    return props.bans?.[index]?.name
+const checkBan = (index: number) => {
+    return !!props.bans?.[index]
 }
+
+const orderedBans = computed(() => (props.side === "blue" ? props.bans?.slice().reverse() : props.bans))
 </script>
+<template>
+    <div class="flex w-full justify-center pt-5">
+        <div
+            v-for="i in 5"
+            class="relative mr-2 aspect-square w-10 border-2 border-gray-400 bg-black"
+            :key="orderedBans?.[i - 1]?.name"
+        >
+            <div v-if="checkBan(i - 1)">
+                <img
+                    :key="orderedBans?.[i - 1]?.name"
+                    :src="orderedBans?.[i - 1]?.image"
+                    :alt="orderedBans?.[i - 1]?.name"
+                />
+            </div>
+            <div class="absolute top-1/2 left-1/2 w-2/3 -translate-x-1/2 -translate-y-1/2" v-else>
+                <BanIcon />
+            </div>
+        </div>
+    </div>
+</template>
