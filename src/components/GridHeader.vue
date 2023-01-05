67a3bf5cfd9d54d50b0a5350d9be7a4a4d4c9440
Corpella <alessandrocordella97@gmail.com>
2023-01-05 23:07:36 +0100

    feat: moved script as first tag, reduced unneccesary tags, improved accessibility and typed events

--- src/components/GridHeader.vue
+++ src/components/GridHeader.vue
@@ -1,44 +1,8 @@
-<template>
-    <div class="w-full flex mb-2">
-        <div class="w-1/2" />
-        <div class="flex w-1/2">
-            <div class="w-2/5"></div>
-            <div class="w-3/5 mr-16">
-                <div class="flex w-full relative">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        class="h-6 w-6 absolute top-1/2 left-0 ml-1 -translate-y-1/2"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                    >
-                        <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="{2}"
-                            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                        />
-                    </svg>
-                    <input
-                        v-model="queryFilter"
-                        class="shadow bg-transparent border-yellow-200 appearance-none border rounded w-full py-2 px-3 text-gray-200 leading-tight h-8 focus:outline-none focus:shadow-outline pl-10"
-                        type="text"
-                        placeholder="Search"
-                    />
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="mr-16 ml-14">
-        <Divider />
-    </div>
-</template>
-
 <script lang="ts" setup>
 import { ref, watch } from "vue"
 import Divider from "./Divider.vue"
 
-const emit = defineEmits(["queryUpdated"])
+const emit = defineEmits<{ (event: "queryUpdated", value: string): void }>()
 
 const queryFilter = ref("")
 
@@ -46,3 +10,32 @@ watch(queryFilter, (val: string): void => {
     emit("queryUpdated", val)
 })
 </script>
+<template>
+    <header class="flex w-full justify-end pb-2">
+        <div class="relative flex w-2/5 self-end" role="search">
+            <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="absolute top-1/2 left-0 ml-1 h-6 w-6 -translate-y-1/2"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+            >
+                <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="{2}"
+                    d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+            </svg>
+            <label class="sr-only" for="search">Search</label>
+            <input
+                v-model="queryFilter"
+                name="search"
+                class="focus:shadow-outline h-8 w-full appearance-none rounded border border-yellow-200 bg-transparent py-2 px-3 pl-10 leading-tight text-gray-200 shadow focus:outline-none"
+                type="text"
+                placeholder="Search"
+            />
+        </div>
+    </header>
+    <Divider class="ml-3" />
+</template>
