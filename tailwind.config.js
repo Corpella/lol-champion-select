f383dcea6ee6ea3462a1f807c39d488904b1fee0
Corpella <alessandrocordella97@gmail.com>
2023-01-05 23:02:16 +0100

    chore: update tailwindconfig file to new syntax

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    purge: { content: ["./public/**/*.html", "./src/**/*.vue"] },
+    content: ["./public/**/*.html", "./src/**/*.vue"],
     theme: {
         extend: {},
     },
