dab248cd9c20fc9fce8e05c7a179d79e37704aad
Corpella <alessandrocordella97@gmail.com>
2023-01-05 23:04:35 +0100

    feat: move script first, improve semantics, remove unneccesary nodes and imports

--- src/components/TeamPicks.vue
+++ src/components/TeamPicks.vue
@@ -1,42 +1,7 @@
-<template>
-    <div class="mt-5 flex w-full flex-col" :class="side == 'blue' ? 'ml-10 self-start pr-10' : 'mr-10 self-end pl-10'">
-        <Divider rhombus-visible :side="side" />
-        <div v-for="i in 5" :key="i">
-            <div class="flex" :class="{ 'flex-row-reverse': position == 'right' }">
-                <template v-if="champions && champions[i - 1]">
-                    <div>
-                        <ChampionsPortrait hideName rounded :value="champions[i - 1]" :side="side" />
-                    </div>
-                    <div class="self-center">
-                        <p class="font-bold">
-                            {{ firstLetterUppercase(champions[i - 1].name) }}
-                        </p>
-                        <p class="">Summoner {{ i }}</p>
-                    </div>
-                </template>
-                <template v-else>
-                    <div
-                        class="mx-5 aspect-square w-20 rounded-full bg-black"
-                        :class="{
-                            'border-2 border-yellow-400': side == 'blue',
-                            'border-2 border-red-700': side == 'red',
-                        }"
-                    />
-                    <div class="self-center">
-                        <p class="font-bold">Something...</p>
-                        <p class="">Summoner {{ i }}</p>
-                    </div>
-                </template>
-            </div>
-            <Divider rhombus-visible :side="side" />
-        </div>
-    </div>
-</template>
-
 <script lang="ts" setup>
 import { PropType } from "vue"
 
-import { Side, Champions, Position } from "@/types/championSelect.types"
+import { Side, Champions, Position } from "~/championSelect"
 import ChampionsPortrait from "./ChampionPortrait.vue"
 import Divider from "./Divider.vue"
 
@@ -48,3 +13,29 @@ defineProps({
 
 const firstLetterUppercase = (name: string): string => name.charAt(0).toUpperCase() + name.slice(1)
 </script>
+
+<template>
+    <div class="flex w-full flex-col pt-5" :class="side === 'blue' ? 'self-start pl-10 pr-10' : 'self-end pr-10 pl-10'">
+        <Divider rhombus-visible :side="side" />
+        <template v-for="i in 5" :key="i">
+            <div class="flex" :class="{ 'flex-row-reverse': position === 'right' }">
+                <ChampionsPortrait v-if="champions?.[i - 1]" hideName rounded :value="champions[i - 1]" :side="side" />
+                <div
+                    v-else
+                    class="mx-5 aspect-square w-20 rounded-full bg-black"
+                    :class="{
+                        'border-2 border-yellow-400': side == 'blue',
+                        'border-2 border-red-700': side == 'red',
+                    }"
+                />
+                <div class="self-center">
+                    <h2 class="font-bold">
+                        {{ champions?.[i - 1] ? firstLetterUppercase(champions[i - 1].name) : "Something..." }}
+                    </h2>
+                    <h2 class="">Summoner {{ i }}</h2>
+                </div>
+            </div>
+            <Divider rhombus-visible :side="side" />
+        </template>
+    </div>
+</template>
