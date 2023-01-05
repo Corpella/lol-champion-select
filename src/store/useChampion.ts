b5b8d8e1803a8a539d066a23de0a99368df0ab97
Corpella <alessandrocordella97@gmail.com>
2023-01-05 23:03:00 +0100

    chore: rename store file

--- src/store/useChampion.ts
+++ src/store/useChampion.ts
@@ -1,6 +1,6 @@
 import { defineStore } from "pinia";
 
-import { Champions, Side, SingleChampion, } from '@/types/championSelect.types'
+import { Champions, Side, SingleChampion, } from '@/types/championSelect'
 
 export const useChampions = defineStore({
     id: "useChampions",
@@ -22,7 +22,7 @@ export const useChampions = defineStore({
     },
     actions: {
         getChampionList() {
-            fetch("http://ddragon.leagueoflegends.com/cdn/12.11.1/data/en_US/champion.json")
+            fetch("http://ddragon.leagueoflegends.com/cdn/12.23.1/data/en_US/champion.json")
                 .then((res) => res.json())
                 .then((response) => {
                     const championsData = Object.values(response.data) as SingleChampion[]
