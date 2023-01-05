3e6d8e37d363a966dcac6ccca46fc46c2b8073dc
Corpella <alessandrocordella97@gmail.com>
2023-01-05 23:08:51 +0100

    feat: move script as first tag, improve accessibility

--- src/components/ChampionPortrait.vue
+++ src/components/ChampionPortrait.vue
@@ -1,53 +1,11 @@
-<template>
-    <div class="relative">
-        <div class="flex w-full flex-col justify-center">
-            <img
-                class="z-10 aspect-square self-center"
-                :class="[
-                    {
-                        grayscale: disabled || selected,
-                        'rounded-full': rounded,
-                        'border-2 border-yellow-400': side == 'blue',
-                        'border-2 border-red-700': side == 'red',
-                        'border border-red-700': phase == 'ban',
-                    },
-                    clickable && phase && `cursor-pointer hover-effects${phase}`,
-                ]"
-                :key="champion?.name"
-                :src="champion?.image"
-                :alt="champion?.name"
-                :width="imageSize"
-            />
-            <p class="text-center text-sm" v-if="!hideName">{{ champion?.name }}</p>
-            <div
-                v-if="selected && phase == 'pick'"
-                class="overlay absolute top-0 left-1/2 z-50 aspect-square -translate-x-1/2 cursor-pointer rounded-full border-2 border-yellow-400"
-            />
-            <HoverIcon
-                :width="imageSize"
-                v-if="selected"
-                class="absolute top-0 left-1/2 z-50 -translate-x-1/2 cursor-pointer"
-                :color="phase == 'ban' ? '#b91c1c' : '#fbbf24'"
-            />
-            <BanIcon
-                :width="imageSize"
-                v-if="selected && phase == 'ban'"
-                class="absolute top-0 left-1/2 z-50 -translate-x-1/2 cursor-pointer"
-                :color="'#b91c1c'"
-            />
-        </div>
-    </div>
-</template>
-
 <script lang="ts" setup>
 import { computed, PropType } from "vue"
 
-import { ChampionPortrait, Phase, Side } from "@/types/championSelect.types"
+import { ChampionPortrait, Phase, Side } from "~/championSelect"
 
 import HoverIcon from "./icons/HoverIcon.vue"
 import BanIcon from "./icons/BanIcon.vue"
-
-const props = defineProps({
+defineProps({
     champion: Object as PropType<ChampionPortrait>,
     clickable: {
         type: Boolean,
@@ -77,6 +35,46 @@ const props = defineProps({
 const imageSize = 80
 const imageSizePx = computed(() => `${imageSize}px`)
 </script>
+<template>
+    <button class="relative" :disabled="disabled || selected">
+        <div class="flex w-full flex-col justify-center">
+            <img
+                class="z-10 aspect-square self-center"
+                :class="[
+                    {
+                        grayscale: disabled || selected,
+                        'rounded-full': rounded,
+                        'border-2 border-yellow-400': side == 'blue',
+                        'border-2 border-red-700': side == 'red',
+                        'border border-red-700': phase == 'ban',
+                    },
+                    clickable && phase && `cursor-pointer hover-effects${phase}`,
+                ]"
+                :key="champion?.name"
+                :src="champion?.image"
+                :alt="champion?.name"
+                :width="imageSize"
+            />
+            <span class="text-center text-sm" v-if="!hideName">{{ champion?.name }}</span>
+            <div
+                v-if="selected && phase === 'pick'"
+                class="overlay absolute top-0 left-1/2 z-50 aspect-square -translate-x-1/2 cursor-pointer rounded-full border-2 border-yellow-400"
+            />
+            <HoverIcon
+                v-if="selected"
+                class="absolute top-0 left-1/2 z-50 -translate-x-1/2 cursor-pointer"
+                :width="imageSize"
+                :color="phase === 'ban' ? '#b91c1c' : '#fbbf24'"
+            />
+            <BanIcon
+                v-if="selected && phase === 'ban'"
+                class="absolute top-0 left-1/2 z-50 -translate-x-1/2 cursor-pointer"
+                :width="imageSize"
+                :color="'#b91c1c'"
+            />
+        </div>
+    </button>
+</template>
 
 <style lang="scss" scoped>
 .overlay {
